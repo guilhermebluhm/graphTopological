f6a4544e26acb23a930b7246a6f7e94162ee32c0
Guilherme Saraiva Bluhm <80696919+guilhermebluhm@users.noreply.github.com>
2023-04-06 14:47:03 -0300

    include linked list for replace std::vector fifo

--- main.cpp
+++ main.cpp
@@ -2,11 +2,73 @@
 #include <stack>
 #include <vector>
 
+struct node{
+
+    int value;
+    node *proximo;
+
+};
+
+class linkedList{
+
+private:
+    node *head;
+public:
+
+    linkedList(){
+        head = nullptr;
+    }
+
+    void push(int v){
+
+        node *newNode = new node;
+        newNode->value = v;
+        newNode->proximo = nullptr;
+
+        if(head == nullptr){
+            newNode->proximo = head;
+            head = newNode;
+        }
+        else{
+
+            newNode->proximo = head;
+            head = newNode;
+
+        }
+
+    }
+
+    void pop(){
+
+        node *tmp = head;
+        head = tmp->proximo;
+        delete tmp;
+
+    }
+
+    int top(){
+
+        node *tmp = head;
+        return tmp->value;
+
+    }
+
+    bool empty(){
+
+        if(head == nullptr)
+            return true;
+        return false;
+
+    }
+
+
+};
+
 class topological{
 
 private:
 
-    void dfs(int node, std::vector<int> *visited, std::stack<int> &stck, std::vector<int> adj[]){
+    void dfs(int node, std::vector<int> *visited, linkedList &stck, std::vector<int> adj[]){
 
         visited->at(node) = 1;
         for(auto it : adj[node]){
@@ -26,7 +88,7 @@ public:
     std::vector<int> topologicalSort(int vertex, std::vector<int> adjacency[]) {
 
         std::vector<int> *visited = new std::vector<int>(vertex,0);
-        std::stack<int> fifo;
+        linkedList fifo;
         for(int i = 0 ; i < vertex ; i+=1) {
 
             if(!visited->at(i)) {
